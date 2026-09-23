ee368aadb5add8168a498569a24a462ea30d4c92
IagoPablo <iagopablo25@gmail.com>
2026-09-23 17:38:40 -0300

    refactor: remove unused IBGE controller

--- backend/src/ibge/ibge.module.ts
+++ backend/src/ibge/ibge.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
+
 import { HttpModule } from '@nestjs/axios';
-import { IbgeController } from './ibge.controller';
 import { IbgeService } from './ibge.service';
 
 @Module({
   imports: [HttpModule],
-  controllers: [IbgeController],
   providers: [IbgeService],
   exports: [IbgeService],
 })
